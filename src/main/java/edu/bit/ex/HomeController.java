b1c592ee7af8cfde4a7c0b5b71b3880bdadcae07
sunyoungyang <yangsun6750@gmail.com>
2020-08-21 10:48:58 +0900

    no message

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,9 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+
+
+//version1
 @Controller
 public class HomeController {
 	
